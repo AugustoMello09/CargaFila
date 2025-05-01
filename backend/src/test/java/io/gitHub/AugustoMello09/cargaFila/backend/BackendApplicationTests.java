e7d9942bcc951505718b43c63935a6e21071ebde
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-05-01 19:21:34 -0300

    Created backend project

---
+++ backend/src/test/java/io/gitHub/AugustoMello09/cargaFila/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.cargaFila.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
